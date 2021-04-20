877238a8b09f1b21b39c9c1898acf4a8cc5cb966
Forrest Klausmeier <fwklausmeier@gmail.com>
2021-04-20 03:29:14 +0000

    Exercises: easy 4 edits

--- small_problems/easy4/what_century_is_that?.rb
+++ small_problems/easy4/what_century_is_that?.rb
@@ -4,64 +4,51 @@
 
 def century(year)
  century = (year % 100) == 0 ? (year/100) : ((year/100) + 1)
- 
+ century.to_s + ending(century) 
 end
 
-p century(2000)# == '20th'
-p century(2001)# == '21st'
-p century(1965)# == '20th'
-p century(256)# == '3rd'
-p century(5)# == '1st'
-p century(10103)# == '102nd'
-p century(1052)# == '11th'
-p century(1127)# == '12th'
-p century(11201)# == '113th'
-
-
-# last_char = century.slice(-1)
-=begin
-  if (year % 100) == 0
-              (year/100).to_s
-            else 
-              ((year/100) + 1).to_s
-            end
-=end
-=begin
-  last_one = century.slice(-1)
- endings  = { "1" => "st",
-              "2" => "nd",
-              "3" => "rd",
-              "4" => "th" }
- case last_one
-   when  then endings["1"] 
- 
- century
-  
-   
-
-
-=begin
-last_two = { "10".."20" => "th",
-             
-  
-}
-=end
-
-=begin
 def ending(century)
-  if century.length == 1
-  
-            
-            if (century.length == 1)
-              case last_char
-              when "1"             then "st"
-              when "2"             then "nd"
-              when "3"             then "rd"
-              when "0" || "4".."9" then "th"
-              end
-            elsif century.length >= 2 && 
-              
-            end
-  century << ending
+ digits = century % 100
+ if (10..20).cover?(digits)
+  "th"
+ else
+  digits = digits % 10
+  case digits
+  when 1         then "st"
+  when 2         then "nd"
+  when 3         then "rd"
+  when 0 || 4..9 then "th"
+  end
+ end
 end
-=end
+
+p century(2000) == '20th'
+p century(2001) == '21st'
+p century(1965) == '20th'
+p century(256) == '3rd'
+p century(5) == '1st'
+p century(10103) == '102nd'
+p century(1052) == '11th'
+p century(1127) == '12th'
+p century(11201) == '113th'
+
+
+#_____launch_solution______________________
+
+def century(year)
+  century = year / 100 + 1
+  century -= 1 if year % 100 == 0
+  century.to_s + century_suffix(century)
+end
+
+def century_suffix(century)
+  return 'th' if [11, 12, 13].include?(century % 100)
+  last_digit = century % 10
+
+  case last_digit
+  when 1 then 'st'
+  when 2 then 'nd'
+  when 3 then 'rd'
+  else 'th'
+  end
+end
